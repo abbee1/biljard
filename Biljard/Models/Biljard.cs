2ba7d7a1efef197b15ce18c56b0641c24ca798d8
albin arvidsson <albinarvidsson97@hotmail.com>
2018-08-04 14:08:15 +0200

    Added Model

---
+++ Biljard/Models/Biljard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biljard.Models
+{
+    public class Biljard
+    {
+        public Guid Id { get; set; }
+
+        public string Winner { get; set; }
+
+        public int LosserBallsLeft { get; set; }
+
+        public string WinnerColor { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string TimeOFDay { get; set; }
+
+        public string WhoStarted { get; set; }
+
+        public string WhoPuttedFirtBall { get; set; }
+
+        public string WonBecauseEight { get; set; }
+
+        public string WonBecauseEightAndWhite { get; set; }
+    }
+}
